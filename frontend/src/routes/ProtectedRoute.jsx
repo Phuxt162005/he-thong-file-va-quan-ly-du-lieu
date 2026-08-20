081c31ac9fd5baf1afcaa336abac1f57519bab7a
Phuxt162005 <phuxt162005@gmail.com>
2026-08-20 19:00:28 +0700

    sửa protected route

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -3,9 +3,11 @@ import { getAccessToken } from "../utils/authStorage";
 
 export default function ProtectedRoute({ children }) {
   const location = useLocation();
+
   const token = getAccessToken();
+
   if (!token) {
     return <Navigate to="/login" replace state={{ from: location.pathname }} />;
-    return children;
   }
+  return children;
 }
