c35245e0ebfec358494636f0655c525fbe716812
Phuxt162005 <phuxt162005@gmail.com>
2026-08-28 13:21:40 +0700

    sửa activityLogRepository

--- backend/src/repositories/activityLogRepository.js
+++ backend/src/repositories/activityLogRepository.js
@@ -2,10 +2,10 @@ const activityLog = require("../models/ActivityLog");
 
 // tạo bản ghi activity log
 exports.create = (data) => {
-  return ActivityLog.create(data);
+  return activityLog.create(data);
 };
 
 // lấy lịch sử hoạt động của User
 exports.findByUser = (userId) => {
-  return ActivityLog.find({ user: userId }).sort({ createdAt: -1 });
+  return activityLog.find({ user: userId }).sort({ createdAt: -1 });
 };
