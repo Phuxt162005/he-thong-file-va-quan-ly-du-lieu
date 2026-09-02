9fb572b377835e4647f780f111689326d5bdb273
Phuxt162005 <phuxt162005@gmail.com>
2026-09-02 15:47:30 +0700

    sửa nhóm 09

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
@@ -7,7 +8,11 @@ const userSchema = new mongoose.Schema(
     avatar: { type: String, default: null },
     storageUsed: { type: Number, default: 0 },
     storageLimit: { type: Number, default: 5368709120 },
-    role: { type: String, default: "user" },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
   { timestamps: true },
 );
