6f360b11bd43fb05d35bf51a85913e0af08ef8b5
Phuxt162005 <phuxt162005@gmail.com>
2026-09-03 17:33:52 +0700

    sửa nhóm 14

--- backend/src/middleware/uploadMiddleware.js
+++ backend/src/middleware/uploadMiddleware.js
@@ -1,16 +1,5 @@
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "storage/files");
-  },
-
-  filename: (req, file, cb) => {
-    const fileName = `${Date.now()}-${file.originalname}`;
-    cb(null, fileName);
-  },
-});
-
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: { fileSize: 500 * 1024 * 1024 },
